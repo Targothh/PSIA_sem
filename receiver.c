d288a123e1bf457d24e6d454bd8336256bdc6b16
sedlaon8 <sedlaon8@fel.cvut.cz>
2024-05-22 16:33:59 +0200

    add: hash

--- receiver.c
+++ receiver.c
@@ -1,4 +1,5 @@
 #include "packet.h"
+#include "sha256.c"
 int main(int argc, char *argv[]){
     int socket_recv = init_socket();
     struct sockaddr_in sender_data_addr, receiver_addr, sender_ack_addr;
@@ -13,8 +14,9 @@ int main(int argc, char *argv[]){
     struct timeval tv;
     tv.tv_sec = MAX_TIMEOUT;
     tv.tv_usec = 0;
-    fr = fopen("OUTPUT1.jpg", "wb");
-    
+    unsigned char received_hash[SHA256_BLOCK_SIZE];
+    char *file_name = "OUTPUT1.jpg";
+    fr = fopen(file_name, "wb");
     setup_addr(&receiver_addr, RECEIVER_PORT, RECEIVER_ADDRESS); 
     setup_addr(&sender_data_addr, NETDERPER_SENDER_DATA_PORT, SENDER_DATA_ADDRESS);
     setup_addr(&sender_ack_addr, NETDERPER_SENDER_ACK_PORT, SENDER_ACK_ADDRESS);
@@ -22,9 +24,21 @@ int main(int argc, char *argv[]){
 
     setsockopt(socket_recv, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
     printf("Listening on port %d...\n", RECEIVER_PORT);
-
-
-
+    while(1){
+        if(recvfrom(socket_recv, &datagram, sizeof(datagram), 0, (struct sockaddr *) &sender_data_addr, &sender_addr_len) < 0){
+            for(int i = 0; i < SHA256_BLOCK_SIZE; i++) printf("%02x", datagram.data[i]);
+            printf("\n");
+            fprintf(stderr,"Error in receiving data\n");
+            crc = crc32(0L, Z_NULL, 0);     
+            crc = crc32(crc, (const Bytef*) datagram.data, (uInt)(sizeof(datagram.data)));
+        }
+        if(crc == datagram.crc){
+            fprintf(stderr,"Hash received\n");
+            break;
+        }
+    }
+    memcpy(datagram.data, received_hash, SHA256_BLOCK_SIZE);
+    for(int i = 0; i < SHA256_BLOCK_SIZE; i++) printf("%02x", datagram.data[i]);
     while(true){
         setup_addr(&sender_ack_addr, NETDERPER_SENDER_ACK_PORT, SENDER_ACK_ADDRESS);
         if(recvfrom(socket_recv, &datagram, sizeof(datagram), 0, (struct sockaddr *) &sender_data_addr, &sender_addr_len) < 0){
@@ -32,14 +46,14 @@ int main(int argc, char *argv[]){
             continue;
         }
         received = datagram.index;
-        crc = crc32(0L, Z_NULL, 0);     
+        crc = crc32(0L, Z_NULL, 0);
         crc = crc32(crc, (const Bytef*) datagram.data, (uInt)(sizeof(datagram.data)));
         if((datagram.index == expected_index && crc == datagram.crc) || received < expected_index){  
             if (received == expected_index){
                 fwrite(datagram.data, sizeof(datagram.data) - datagram.free_space, 1, fr);
                 fprintf(stderr,"Received %d\n", received);
                 sendto(socket_recv, &expected_index, sizeof(expected_index), 0, (struct sockaddr *) &sender_ack_addr, sender_ack_addr_len);
-                expected_index++;
+                expected_index++;                
                 if (datagram.free_space != 0)
                     break;  
             }
@@ -50,7 +64,27 @@ int main(int argc, char *argv[]){
             sendto(socket_recv, &nack, sizeof(nack), 0, (struct sockaddr *) &sender_ack_addr, sender_ack_addr_len);
         }
     }
+
     fclose(fr);
+    SHA256_CTX hash;
+    sha256_init(&hash);
+    int size;
+    unsigned char data[DATA_SIZE];
+    unsigned char final_hash[SHA256_BLOCK_SIZE];
+    FILE *fhs;
+    if ((fhs = fopen(file_name, "rb")) == NULL){
+        fprintf(stderr,"File not found!\n");
+        exit(EXIT_NOT_FOUND);
+    }
+    while ((size = fread (data, 1, DATA_SIZE, fhs)) != 0){
+        sha256_update(&hash, data, size);
+    }
+    sha256_final(&hash, final_hash);
+    fclose(fhs);
+    printf("File hash:\n");
+    for(int i = 0; i < SHA256_BLOCK_SIZE; i++) printf("%02x", final_hash[i]);
+    printf("Received hash:\n");
+    for(int i = 0; i < SHA256_BLOCK_SIZE; i++) printf("%02x", received_hash[i]);
     close(socket_recv);
     return 0;
 } 
--- sender.c
+++ sender.c
@@ -1,4 +1,32 @@
 #include "packet.h"
+#include "sha256.c"
+
+void read_data(FILE *fw, datagram_t *datagram){
+    int read_data;
+    if((read_data=fread(datagram->data, 1, sizeof(datagram->data), fw)) != DATA_SIZE){
+        datagram->free_space = sizeof(datagram->data) - read_data;
+    } 
+}
+int send_data( int socket_data_sender, struct sockaddr_in receiver_addr, datagram_t *datagram){
+    uLong crc = crc32(0L, Z_NULL, 0);                                        
+    datagram->crc = crc32(crc, (const Bytef*) datagram->data, (uInt)(sizeof(datagram->data)));     
+    if(sendto(socket_data_sender, datagram, sizeof(*datagram), 0, (struct sockaddr *) &receiver_addr, sizeof(receiver_addr)) < 0){
+        fprintf(stderr,"Failed to send data\n");
+        return -1;
+    }
+    return datagram->index;
+}
+
+int recv_ack(int socket_ack_sender, struct sockaddr_in receiver_addr,  int sent, datagram_t* datagram){
+    int ack = -1;
+    socklen_t receiver_addr_len = sizeof(receiver_addr);
+    if(recvfrom(socket_ack_sender, &ack, sizeof(ack), 0, (struct sockaddr *) &receiver_addr , &receiver_addr_len) >= 0 && ack == sent){
+        datagram->index++;
+    } else {
+        fprintf(stderr,"NACK or timeout, resending %d, ack = %d\n", sent, ack);
+    }
+    return ack;
+}
 
 int main(int argc, char *argv[]){
     char name[32];
@@ -6,7 +34,23 @@ int main(int argc, char *argv[]){
     int socket_data_sender = init_socket(),
         socket_ack_sender = init_socket();
     struct sockaddr_in sender_data_addr, receiver_addr, sender_ack_addr;
-    socklen_t receiver_addr_len = sizeof(receiver_addr);
+    SHA256_CTX hash;
+    sha256_init(&hash);
+    int size;
+    unsigned char data[DATA_SIZE];
+    unsigned char final_hash[SHA256_BLOCK_SIZE];
+    FILE *fhs;
+    if ((fhs = fopen(name, "rb")) == NULL){
+        fprintf(stderr,"File not found!\n");
+        exit(EXIT_NOT_FOUND);
+    }
+    while ((size = fread (data, 1, DATA_SIZE, fhs)) != 0){
+        sha256_update(&hash, data, size);
+    }
+    sha256_final(&hash, final_hash);
+    fclose(fhs);
+    for(int i = 0; i < SHA256_BLOCK_SIZE; i++) printf("%02x", final_hash[i]);
+
     datagram_t datagram;
     datagram.index = 0;
     datagram.free_space = 0;
@@ -26,31 +70,25 @@ int main(int argc, char *argv[]){
     tv.tv_usec = 0;
     setsockopt(socket_data_sender, SOL_SOCKET, SO_SNDTIMEO, (const char*)&tv, sizeof tv);
     setsockopt(socket_ack_sender, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
-    int read_data; 
-    uLong crc;
     int ack = -1;
     int sent = -1;
+    memcpy(datagram.data, final_hash, SHA256_BLOCK_SIZE);
+    while(!ack){
+        send_data(socket_data_sender, receiver_addr, &datagram);
+        fprintf(stderr,"Sending hash\n");
+        ack = recv_ack(socket_ack_sender, receiver_addr, sent, &datagram);
+    }
+        for(int i = 0; i < SHA256_BLOCK_SIZE; i++) printf("%02x", datagram.data[i]);
+
     while(true){
         setup_addr(&receiver_addr, NETDERPER_RECEIVER_PORT, NETDERPER_RECEIVER_ADDRESS);
         if (ack == sent){
-            if((read_data=fread(datagram.data, 1, sizeof(datagram.data), fw)) != DATA_SIZE){
-                datagram.free_space = sizeof(datagram.data) - read_data;
-            } 
+            read_data(fw, &datagram);
         }
-        crc = crc32(0L, Z_NULL, 0);                                        
-        datagram.crc = crc32(crc, (const Bytef*) datagram.data, (uInt)(sizeof(datagram.data)));     
-        if(sendto(socket_data_sender, &datagram, sizeof(datagram), 0, (struct sockaddr *) &receiver_addr, sizeof(receiver_addr)) < 0){
-            fprintf(stderr,"Failed to send data\n");
-            continue;
-        }
-        sent = datagram.index;
-        fprintf(stderr,"Sent %d\n", sent);
-        //todo crc
-        if(recvfrom(socket_ack_sender, &ack, sizeof(ack), 0, (struct sockaddr *) &receiver_addr , &receiver_addr_len) >= 0 && ack == sent){
-            datagram.index++;
-        } else {
-            fprintf(stderr,"NACK or timeout, resending %d, ack = %d\n", sent, ack);
-        }
+
+        sent = send_data(socket_data_sender, receiver_addr, &datagram);
+        fprintf(stderr,"Sent %d\n", sent);  
+        ack = recv_ack(socket_ack_sender, receiver_addr, sent, &datagram);
         if (ack == sent && datagram.free_space != 0)
             break;
     }
@@ -58,5 +96,6 @@ int main(int argc, char *argv[]){
     fclose(fw);
     close(socket_data_sender);
     close(socket_ack_sender);
+    sha256_final(&hash, final_hash);
     return 0;
 }
